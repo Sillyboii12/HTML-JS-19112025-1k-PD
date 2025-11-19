1e2b46c85b8ebee47f8206cf277433ff6ad331b5
unknown <deivis.stasuls@gmail.com>
2025-11-19 14:55:40 +0200

    connected and set up external js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// your code goes here
+// console.log("Js");
+
+const button = document.getElementById("calc");
+const output = document.getElementById("output");
+const height = document.getElementById("height");
+const mass = document.getElementById("mass");
+
+
+button.addEventListener('click', function calc(){
+    console.log("clicked");
+});
