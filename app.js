8318733de9ae4c5616e15b25952221d9c17971e5
unknown <deivis.stasuls@gmail.com>
2025-11-19 15:06:42 +0200

    BMI calc finished

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const height = document.getElementById("height");
 const mass = document.getElementById("mass");
 
 
-button.addEventListener('click', function calc(){
-    console.log("clicked");
+button.addEventListener('click', function calcBMI(){
+    // console.log("clicked");
+    let result = "";
+
+    let BMI = mass.value / (height.value / 100) **2;
+
+    if (BMI > 30) {
+        result = "(Aptaukošanās)";
+    } else if (BMI < 20) {
+        result = "(Nepietiekama ķermeņa masa)";
+    } else {
+        result = "(Normālā ķermeņa masa)";
+    }
+
+    output.innerHTML = `Jūsu ĶMI ir ${BMI.toFixed(1)} ${result}.`;
 });
