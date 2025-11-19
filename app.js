efe2a34d6899735f191c8182ad1f1c7b178f2fc0
unknown <deivis.stasuls@gmail.com>
2025-11-19 15:27:54 +0200

    calc history and upgraded condition checker

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const button = document.getElementById("calc");
 const output = document.getElementById("output");
 const height = document.getElementById("height");
 const mass = document.getElementById("mass");
+const historyList = document.getElementById("history");
 
 
 button.addEventListener('click', function calcBMI(){
@@ -14,11 +15,18 @@ button.addEventListener('click', function calcBMI(){
 
     if (BMI > 30) {
         result = "(Aptaukošanās)";
-    } else if (BMI < 20) {
+    } else if (BMI < 18.5) {
         result = "(Nepietiekama ķermeņa masa)";
+    } else if (BMI >= 18.5 && BMI <= 24.99) {
+        result = "(Normāla ķermeņa masa)";
     } else {
-        result = "(Normālā ķermeņa masa)";
+        result = "(Lieka ķermeņa masa)";
     }
 
     output.innerHTML = `Jūsu ĶMI ir ${BMI.toFixed(1)} ${result}.`;
+
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `Jūsu ĶMI ir ${BMI.toFixed(1)} ${result}. Masa: ${mass.value} kg, Augums: ${height.value} cm.`;
+    historyList.appendChild(listItem);
+    // historyList.insertBefore(listItem, historyList.firstChild); -- šis būtu labāk lai jaunā vēsture paliek augšā un vecā iet lejā;
 });
